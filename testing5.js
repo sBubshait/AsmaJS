8fd11997f556d9e542a66386dad7fd19b73c92b9
Saleh Bubshait <44058159+sBubshait@users.noreply.github.com>
2023-07-11 14:59:28 +0300

    Delete testing5.js

--- testing5.js
+++
@@ -1,9 +0,0 @@
-import tokenize from './src/tokenizer.js';
-import parse from './src/parser.js';
-
-var code = `toInteger(prompt("Enter a number: "))`
-
-var tokens = tokenize(code);
-var ast = parse(tokens);
-
-console.log(JSON.stringify(ast, null, 2));
